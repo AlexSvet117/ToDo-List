3ae65253301de699305bd4b9e46814b58f219f66
AlexSvet117 <alex.svet117@gmail.com>
2025-02-02 02:06:55 +0000

    Final upates

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -4,7 +4,7 @@ import Task from './Task'
 function TaskList({tasks, removeTask, toggleComplete}) {
 
 
-  return (tasks.length < 1 ? (<div className='bg-bone-white text-dark text-center mt-2 mx-auto col-5'>No Tasks Added</div>) : 
+  return (tasks.length < 1 ? (<div className='bg-bone-white text-center mt-2 p-2 mx-auto col-5'>No Tasks Added</div>) : 
 
     (<div className="d-flex flex-column gap-1 align-items-center">
         {tasks.map((task)=> (
